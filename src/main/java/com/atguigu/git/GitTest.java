06ab4f1c8dc1bab94c001566f23520f479f1e5bd
Arglone <1443485536@qq.com>
2022-08-30 14:42:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Arglone
+ * @create 2022-08-30 14:28
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
