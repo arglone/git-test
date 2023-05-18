25d04d2f38c1df5ccd32f4aef93d38c0d1e27965
Arglone <1443485536@qq.com>
2023-05-18 15:01:23 +0800

    feature/1 push1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,5 +16,7 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee test2");
+
+        System.out.println("feature/1");
     }
 }
